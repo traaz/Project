204f06152faa4aa75654c3de7df8daa8bbfad72f
traaz <angiyaz0@gmail.com>
2021-12-16 21:38:58 +0300

    .

--- Entities/Concrete/Ilac.cs
+++ Entities/Concrete/Ilac.cs
@@ -10,5 +10,6 @@ namespace Entities.Concrete
         public int Id { get; set; }
         public string IlacIsmi { get; set; }
         public int HastalikId { get; set; }
+        public int CalisanId { get; set; }
     }
 }
