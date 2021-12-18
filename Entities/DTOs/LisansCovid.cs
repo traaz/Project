13d8bc62190f486042faf0d601eceacf170295e5
traaz <angiyaz0@gmail.com>
2021-12-18 11:58:57 +0300

    .

---
+++ Entities/DTOs/LisansCovid.cs
@@ -0,0 +1,12 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.DTOs
+{
+  public  class LisansCovid:IDto
+    {
+        public int CalisanId { get; set; }
+    }
+}
